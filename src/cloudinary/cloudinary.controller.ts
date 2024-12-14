64d3a9e75f6026725e086bdfb4a03f0016e099c8
Faruq-Hameed <faruqhameed1@gmail.com>
2024-12-14 21:47:35 +0100

    file uploaded succesfully

--- src/cloudinary/cloudinary.controller.ts
+++ src/cloudinary/cloudinary.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, UseInterceptors, UploadedFile, UploadedFiles } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Public } from 'src/common/guards/public.guard';
 
 @Controller('images')
 export class CloudinaryController {
@@ -8,9 +9,11 @@ export class CloudinaryController {
     private readonly cloudinaryService: CloudinaryService
   ) {}
 
+  @Public()
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   async uploadImage(@UploadedFile() file: Express.Multer.File) {
+    console.log("file to upload is : ",{file});
     const uploadResponse =  await this.cloudinaryService.uploadFile(file);
     console.log({uploadResponse})
     return {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import multer from 'multer';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }));
+  // app.use(multer().single('file'));
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
