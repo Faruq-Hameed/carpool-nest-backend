ddf28c8ec40a0cb2d6603626507d6c71793fa2f6
Faruq-Hameed <faruqhameed1@gmail.com>
2024-12-14 20:39:59 +0100

    set up files uploader module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CloudinaryService } from './cloudinary/cloudinary.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+    private readonly cloudinaryService: CloudinaryService
+  ) {}
+
 
   @Get()
   getHello(): string {
--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
-import { Cloudinary } from './cloudinary';
 import { CloudinaryProvider } from './cloudinary.provider';
 
 @Module({
-  providers: [CloudinaryProvider,CloudinaryService, Cloudinary],
-  exports: [CloudinaryProvider,CloudinaryService, Cloudinary],
+  providers: [CloudinaryProvider,CloudinaryService,],
+  exports: [CloudinaryProvider,CloudinaryService,],
 })
 export class CloudinaryModule {}
