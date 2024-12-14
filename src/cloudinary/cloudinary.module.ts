ca584193398fae93aa62571fc02ffb9a8f669b1f
Faruq-Hameed <faruqhameed1@gmail.com>
2024-12-14 16:03:37 +0100

    set up files uploader module

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
 import { Cloudinary } from './cloudinary';
+import { CloudinaryProvider } from './cloudinary.provider';
 
 @Module({
-  providers: [CloudinaryService, Cloudinary]
+  providers: [CloudinaryProvider,CloudinaryService, Cloudinary],
+  exports: [CloudinaryProvider,CloudinaryService, Cloudinary],
 })
 export class CloudinaryModule {}
