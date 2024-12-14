f271002dcdddb117c5b18b0a857371b87a6f8802
Faruq-Hameed <faruqhameed1@gmail.com>
2024-12-14 21:48:49 +0100

    file uploaded succesfully

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -9,3 +9,25 @@ import { CloudinaryController } from './cloudinary.controller';
   exports: [CloudinaryProvider,CloudinaryService,],
 })
 export class CloudinaryModule {}
+
+
+// // cloudinary.module.ts
+// import { Module } from '@nestjs/common';
+// import { MulterModule } from '@nestjs/platform-express';
+// import { CloudinaryService } from './cloudinary.service';
+// import { CloudinaryProvider } from './cloudinary.provider';
+// import { CloudinaryController } from './cloudinary.controller';
+
+// @Module({
+//   imports: [
+//     MulterModule.register({
+//       limits: {
+//         fileSize: 10 * 1024 * 1024, // 10MB max file size
+//       },
+//     }),
+//   ],
+//   providers: [CloudinaryProvider, CloudinaryService],
+//   controllers: [CloudinaryController],
+//   exports: [CloudinaryProvider, CloudinaryService],
+// })
+// export class CloudinaryModule {}
