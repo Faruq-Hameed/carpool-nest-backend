f67479a8ea60d1594fa59f2b6ad3d43b2be75018
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-05-03 17:47:22 +0100

    updated otp module

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,6 +3,7 @@ import { registerAs } from "@nestjs/config";
 export default registerAs('database', () => ({
     type: 'postgres',
     host: process.env.DB_HOST || 'localhost',
+    port:process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
--- src/modules/otps/otps.module.ts
+++ src/modules/otps/otps.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Otp } from './entities/otp.entity';
 import { OtpService } from './otps.service';
+import { UsersModule } from '../users/users.module';
+import { OtsController } from './otps.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Otp])],
+  imports: [TypeOrmModule.forFeature([Otp]), UsersModule],
+  controllers: [OtsController],
   providers: [OtpService],
   exports: [OtpService],
 })
