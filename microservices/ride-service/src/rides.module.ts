5ca3734036370fe64234c903c7f9cdb6243ebb44
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-05-23 22:11:16 +0100

    sepeerated all services and installed deps

--- microservices/ride-service/src/rides.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RidesService } from './rides.service';
-import { RidesController } from './rides.controller';
-
-@Module({
-  controllers: [RidesController],
-  providers: [RidesService],
-})
-export class RidesModule {}
